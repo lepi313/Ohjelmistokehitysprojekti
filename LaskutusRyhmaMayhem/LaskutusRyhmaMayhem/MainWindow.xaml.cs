386a63e9bc31813cb0c2303271e18d656a53df12
lepi313 <jarno22055@student.hamk.fi>
2023-03-01 15:03:13 +0200

    aloittelu Main and Serviceview

--- LaskutusRyhmaMayhem/LaskutusRyhmaMayhem/MainWindow.xaml.cs
+++ LaskutusRyhmaMayhem/LaskutusRyhmaMayhem/MainWindow.xaml.cs
@@ -24,5 +24,19 @@ namespace LaskutusRyhmaMayhem
         {
             InitializeComponent();
         }
+
+        private void palvelutasot_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Maksut_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
+
+
+        
+    
+
