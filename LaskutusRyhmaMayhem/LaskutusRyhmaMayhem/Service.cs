a5e9bb075511350aade47f42a18a2da1236db18c
jaanahp <pusa.jaana@gmail.com>
2023-03-02 19:15:59 +0200

    korjattu Service-luokan mergen aiheuttamat ongelmat

--- LaskutusRyhmaMayhem/LaskutusRyhmaMayhem/Service.cs
+++ LaskutusRyhmaMayhem/LaskutusRyhmaMayhem/Service.cs
@@ -14,14 +14,8 @@ namespace LaskutusRyhmaMayhem
 {
     class Service
     {
-<<<<<<< HEAD
-
-        public string ServLevel { get; set; }
-        public double MPrice { get; set; }
-=======
         public double Hinta { get; set; }
         public string Text { get; set; }
->>>>>>> 68dfee29bbfbc958ac7b6e400d9ffc0f74fb1368
 
         public Service(double hinta, string text)
         {
@@ -34,11 +28,3 @@ namespace LaskutusRyhmaMayhem
 }
 
 
-<<<<<<< HEAD
-
-      
-    
-
-
-=======
->>>>>>> 68dfee29bbfbc958ac7b6e400d9ffc0f74fb1368
